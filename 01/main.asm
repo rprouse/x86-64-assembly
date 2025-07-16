3fda9ea6c484654bfa9276740375d132a32fe9f1
Rob Prouse <rob.prouse@aspiresoftware.com>
2025-07-16 11:56:12 -0400

    Fix NASM extension warnings

--- 01/main.asm
+++ 01/main.asm
@@ -1,11 +1,12 @@
 ; main.asm — Minimal PE executable for Windows (x86-64)
 ; Calls INT 3 (software breakpoint), then returns using ExitProcess(0)
 
+[bits 64]
 global main
 extern ExitProcess
 
 section .text
 main:
     int3                    ; trigger a debugger breakpoint
-    xor ecx, ecx            ; exit code 0
+    xor rcx, rcx            ; exit code 0
     call ExitProcess        ; call Windows API to exit
